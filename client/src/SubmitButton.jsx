12251b12e3682ea6d299f7e51419c0c52e968ca2
lgang2 <34961729+lgang2@users.noreply.github.com>
2023-06-22 16:00:13 -0400

    First commit, Basic frontend for user login done

---
+++ client/src/SubmitButton.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+class SubmitButton extends React.Component{
+
+  render() {
+    return (
+      <div className="submitButton">
+          
+        <button
+          className="btn"
+          disabled={this.props.disabled}
+          onClick= { () => this.props.onClick() }
+        >
+
+          {this.props.text}
+        </button>
+
+      </div>
+    );
+  }
+
+}
+
+export default SubmitButton
