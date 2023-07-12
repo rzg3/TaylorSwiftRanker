930f065b83cdb5e98e7de5ad791a3aa8032bc6d3
lgang2 <34961729+lgang2@users.noreply.github.com>
2023-07-11 23:12:28 -0400

    css changes for mobile centering

--- server/client/src/Rankings.jsx
+++ server/client/src/Rankings.jsx
@@ -42,7 +42,7 @@ function Rankings(props) {
 
     return (
       <>
-        <Container className="p-2" style={{"width": "100vw"}} align="center">
+        <Container className="p-2" align="center">
         <div className='d-inline-flex flex-column justify-content-between align-content-center flex-wrap m-5' style={{boxSizing: 'border-box', width: 'calc(100% - 6rem)'}}>
         <div className='mb-3' style={{boxSizing: 'border-box', width: '100%'}}>
           <h2>{props.display} Rankings</h2>
